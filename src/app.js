3e522520d908f4218fa4132038b95a00aac96314
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-09 21:16:52 +0200

    feat: add express server

---
+++ src/app.js
@@ -0,0 +1,10 @@
+// Copyright (C) 2018 Quentin VERLHAC
+
+// Import node package modules
+const express = require('express');
+
+// Instantiate express server
+const app = express();
+
+// Exports
+module.exports = app;
