d80f6e857b535879c706db2346871f3f934ea59d
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-13 20:55:57 +0200

    feat: addWebsite controller

---
+++ src/website/websiteController.js
@@ -0,0 +1,26 @@
+// Copyright (C) 2018 Quentin VERLHAC
+
+// Import node package modules
+
+const { Website } = require('../database/database');
+
+async function addWebsite(req, res, next) {
+  try {
+    const website = new Website({
+      name: req.body.name,
+      url: req.body.url,
+      timeInterval: req.body.timeInterval,
+    });
+    // Save the website
+    website.save();
+    res.send(website);
+  } catch (err) {
+    next(err);
+  }
+}
+
+
+module.exports = {
+  addWebsite,
+};
+
