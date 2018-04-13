f8cfebfb12d9f2eb31885065f5f224b7489e21c1
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-13 17:08:20 +0200

    feat: add models creation

---
+++ src/database/database.js
@@ -0,0 +1,29 @@
+// Copyright (C) 2018 Quentin VERLHAC
+
+// Import node package modules
+const mongoose = require('mongoose');
+
+const { websiteSchema, historySchema } = require('./schemas');
+
+// Connect mongoose to the mongodb database
+mongoose.connect('mongodb://puppy-db/test');
+
+const db = mongoose.connection;
+
+// Test if there is errors during the connection
+db.on('error', console.error.bind(console, 'connection error:'));
+
+// Once the connection is opened
+db.once('open', () => {
+  console.log('Successful connection to database');
+
+  // Create models from mongodb schema
+  const website = db.model('website', websiteSchema);
+  const history = db.model('history', historySchema);
+
+  // Export models
+  module.exports = {
+    website,
+    history,
+  };
+});
