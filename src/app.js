99c51e28a8d0aa9ed783b252b0d5c12f595e7115
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-13 21:29:27 +0200

    feat: set up website router

--- src/app.js
+++ src/app.js
@@ -2,9 +2,16 @@
 
 // Import node modules
 const express = require('express');
+const bodyParser = require('body-parser');
+const websiteRouter = require('./website/websiteRouter');
 
 // Instantiate express server
 const app = express();
+
+// Use body parser for parsing http requests
+app.use(bodyParser.json());
+
+app.use('/api/website', websiteRouter);
 
 // Exports
 module.exports = app;
