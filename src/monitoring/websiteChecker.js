f8b70e8db09bca2bcdd5ea57497dbc7421d9cb51
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-14 20:52:49 +0200

    fix: timeout in website checker

--- src/monitoring/websiteChecker.js
+++ src/monitoring/websiteChecker.js
@@ -7,7 +7,7 @@ function checkWebsite(website) {
     // Save beginning time to compute response time.
     const beginningTime = Date.now();
     // Send the request with a timeout of 10 seconds.
-    axios.get(website.url, { timeout: 1 })
+    axios.get(website.url, { timeout: 10000 })
       .then(async (response) => {
         // Success, everything is fine
         const receptionTime = Date.now();
