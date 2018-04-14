db093e9d091cd7deb03d52500cc37ce1f08d8a02
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-14 20:50:22 +0200

    feat: add error management in check website

--- src/monitoring/websiteChecker.js
+++ src/monitoring/websiteChecker.js
@@ -1,15 +1,47 @@
 // Import node modules
 const axios = require('axios');
 
-async function checkWebsite(website) {
-  console.log(website);
-  const response = await axios.get(website.url);
-  website.checkList.push({
-    date: Date.now(),
-    answered: true,
-    responseTime: 1,
-    responseCode: response.status,
-  });
+// This function sends a request to the specified website to check if it is available.
+function checkWebsite(website) {
+  try {
+    // Save beginning time to compute response time.
+    const beginningTime = Date.now();
+    // Send the request with a timeout of 10 seconds.
+    axios.get(website.url, { timeout: 1 })
+      .then(async (response) => {
+        // Success, everything is fine
+        const receptionTime = Date.now();
+        // Add the information to the website check list
+        website.checkList.push({
+          date: receptionTime,
+          answered: true,
+          responseTime: receptionTime - beginningTime,
+          responseCode: response.status,
+        });
+        await website.save();
+      })
+      .catch(async (error) => {
+        // There is an error
+        const receptionTime = Date.now();
+        let responseCode;
+        let responseTime;
+        if (error.response) {
+          // If there is a response, get its code and its time
+          responseCode = error.response.status;
+          responseTime = receptionTime - beginningTime;
+        }
+        // Add the information to the website check list
+        website.checkList.push({
+          date: receptionTime,
+          answered: false,
+          responseTime,
+          responseCode,
+        });
+        await website.save();
+      });
+  } catch (err) {
+    console.error(err);
+  }
 }
 
 module.exports = checkWebsite;
