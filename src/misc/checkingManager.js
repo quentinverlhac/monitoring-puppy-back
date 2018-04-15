69ac8c7893a9db72ed93b3fa0397d666a213fd9a
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-15 21:52:21 +0200

    refactor: move checking functions

---
+++ src/misc/checkingManager.js
@@ -0,0 +1,31 @@
+// Import node modules
+const checkWebsite = require('../misc/websiteChecker');
+const { Website } = require('../database/database');
+
+// This global variable keeps track of running intervals
+const intervals = [];
+
+// This function set up checking of websites
+async function startChecking() {
+  // Get all websites
+  const websites = await Website.find().select('url checkInterval checkList');
+  // Check website availability at regular intervals, given by the checkInterval field
+  websites.map((website) => {
+    const interval = setInterval(checkWebsite, website.checkInterval * 1000, website);
+    // Keep track of running intervals to be able to cancel them later
+    intervals.push(interval);
+  });
+}
+
+// This function stops checking of websites
+async function stopChecking() {
+  // Cancel all running intervals
+  intervals.map((interval) => {
+    clearInterval(interval);
+  });
+}
+
+module.exports = {
+  startChecking,
+  stopChecking,
+};
--- src/monitoring/monitoringController.js
+++ src/monitoring/monitoringController.js
@@ -1,20 +1,11 @@
 // Import node modules
-const checkWebsite = require('../misc/websiteChecker');
-const { Website } = require('../database/database');
-
-const intervals = [];
+const { startChecking, stopChecking } = require('../misc/checkingManager');
 
 async function runMonitoring(req, res, next) {
   try {
     console.log('Monitoring is running');
-    // Get all websites
-    const websites = await Website.find().select('url checkInterval checkList');
-    // Check website availability at regular intervals, given by the checkInterval field
-    websites.map((website) => {
-      const interval = setInterval(checkWebsite, website.checkInterval * 1000, website);
-      // Keep track of running intervals to be able to cancel them later
-      intervals.push(interval);
-    });
+    // Start checking websites
+    startChecking();
     res.send('Monitoring is running');
   } catch (err) {
     next(err);
@@ -24,10 +15,8 @@ async function runMonitoring(req, res, next) {
 async function stopMonitoring(req, res, next) {
   try {
     console.log('Monitoring stopped');
-    // Cancel all running intervals
-    intervals.map((interval) => {
-      clearInterval(interval);
-    });
+    // Stop checking websites
+    stopChecking();
     res.send('Monitoring stopped');
   } catch (err) {
     next(err);
