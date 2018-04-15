4777ca510785f14ca24bfc1a09df6b9d9a96b1bc
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-15 13:24:02 +0200

    feat: add website filter

--- src/log/logController.js
+++ src/log/logController.js
@@ -3,11 +3,10 @@ const { Log } = require('../database/database');
 
 async function getLogs(req, res, next) {
   try {
-    console.log(req.params);
     const logs = await Log.find({
       dateTimestamp: { $gt: parseInt(req.params.beginning), $lt: parseInt(req.params.end) },
-    });
-    res.send(logs);
+    }).populate('website');
+    res.send(logs.filter(log => log.website.name === req.params.website));
   } catch (err) {
     next(err);
   }
