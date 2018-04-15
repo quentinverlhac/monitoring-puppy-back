d3d9274ef645fb2666534a42626ca9b474cf563e
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-15 23:20:31 +0200

    feat: add statisticsComputer

---
+++ src/statistics/statisticsComputer.js
@@ -0,0 +1,26 @@
+// Import node modules
+const { Log } = require('../database/database');
+const getLogs = require('../misc/logGetter');
+const computeAvailability = require('./availabilityComputer');
+const { computeMaxResponseTime, computeAverageResponseTime } = require('./responseTimeComputer');
+const countResponseCodes = require('./responseCodeCounter');
+
+async function computeStatistics(websiteName, beginning, end) {
+  // Get the logs of the website between the interval
+  const logs = await getLogs(websiteName, beginning, end);
+  // Compute statistics
+  const averageAvailability = await computeAvailability(logs);
+  const maxResponseTime = await computeMaxResponseTime(logs);
+  const averageResponseTime = await computeAverageResponseTime(logs);
+  const responseCodes = await countResponseCodes(logs);
+  // Display statistics
+  console.log(`Average availability: ${averageAvailability}`);
+  console.log(`Maximum response time: ${maxResponseTime}`);
+  console.log(`Average response time: ${averageResponseTime}`);
+  Object.entries(responseCodes).map((code) => {
+    console.log(`Number of HTTP status ${code[0]} occurences: ${code[1]}`);
+  });
+}
+
+module.exports = computeStatistics;
+
