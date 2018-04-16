fae10f85ba564d318c87d9a7b6b8209bd424a9ee
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-16 23:24:17 +0200

    feat: add history controller

---
+++ src/history/historyController.js
@@ -0,0 +1,20 @@
+// Import node modules
+const { History } = require('../database/database');
+
+async function getHistory(req, res, next) {
+  try {
+    // Get all history until date timestamp
+    const history = await History.find({ dateTimestamp: { $gt: parseInt(req.params.timestamp) } }).populate();
+    res.send(history);
+  } catch (err) {
+    next(err);
+  }
+}
+
+async function addToHistory(event) {
+  const history = await new History(event);
+  history.save();
+}
+
+module.exports = getHistory;
+
