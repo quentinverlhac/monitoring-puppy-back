15dc5abc15f395ea3094265de1b2796d8f4133c1
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-16 14:57:50 +0200

    feat: add socket.io connection

--- src/app.js
+++ src/app.js
@@ -1,19 +1,25 @@
 // Import node modules
-const express = require('express');
+// Instantiate express app
+const app = require('express')();
+// Set up http server with app
+const server = require('http').Server(app);
+// Instantiate socket.io with http serveur
+const io = require('socket.io')(server);
 const bodyParser = require('body-parser');
 const websiteRouter = require('./website/websiteRouter');
 const monitoringRouter = require('./monitoring/monitoringRouter');
 const logRouter = require('./log/logRouter');
 
-// Instantiate express server
-const app = express();
-
-// Use body parser for parsing http requests
+// Use body parser for parsing the body of http requests to json objets
 app.use(bodyParser.json());
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
 
 app.use('/api/website', websiteRouter);
 app.use('/api/monitoring', monitoringRouter);
 app.use('/api/log', logRouter);
 
 // Exports
-module.exports = app;
+module.exports = server;
