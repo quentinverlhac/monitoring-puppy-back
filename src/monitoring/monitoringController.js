7beabc016e3b587861d33e589bfbe8987ae72c28
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-16 17:32:14 +0200

    refactor: remove stat computing from monitoring

--- src/monitoring/monitoringController.js
+++ src/monitoring/monitoringController.js
@@ -1,14 +1,11 @@
 // Import node modules
 const { startChecking, stopChecking } = require('../misc/checkingManager');
-const { startStatisticsComputing, stopStatisticsComputing } = require('../misc/statisticsManager');
 
 async function runMonitoring(req, res, next) {
   try {
     console.log('Monitoring is running');
     // Start checking websites
     startChecking();
-    startStatisticsComputing(10000, 600000);
-    startStatisticsComputing(60000, 3600000);
     res.send('Monitoring is running');
   } catch (err) {
     next(err);
@@ -20,7 +17,6 @@ async function stopMonitoring(req, res, next) {
     console.log('Monitoring stopped');
     // Stop checking websites
     stopChecking();
-    stopStatisticsComputing();
     res.send('Monitoring stopped');
   } catch (err) {
     next(err);
