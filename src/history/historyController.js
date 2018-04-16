6004715bce4523d34cf0f4cf0c169a5f23727c1c
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-17 00:00:45 +0200

    feat: add getAlert

--- src/history/historyController.js
+++ src/history/historyController.js
@@ -1,5 +1,43 @@
 // Import node modules
-const { History } = require('../database/database');
+const { Website, History } = require('../database/database');
+const getLogs = require('../misc/logGetter');
+const computeAvailability = require('../statistics/logic/availabilityComputer');
+
+async function getAlerts(req, res, next) {
+  try {
+    // Measure current date
+    const now = Date.now();
+    // Get every websites
+    const websites = await Website.find();
+    const alerts = websites.map(async (website) => {
+      // Get all logs within last 2 minutes
+      const logs = await logGetter(website, now - 120000, now);
+      // Compute availability of the logs
+      const availability = computeAvailability(logs);
+      // If there is a breakthrough in the website situation
+      if ((availability < 0.8 && !website.isDown) || (availability >= 0.8 && website.isDown)) {
+        // Change the website situation in bdd
+        website.isDown = !website.isDown;
+        await website.save();
+        // Create an alert
+        const alert = new History({
+          status: (website.isDown ? 'down' : 'up'),
+          availability,
+          dateTimestamp: now,
+        });
+        alert.save();
+        // Return the alert
+        return alert;
+      }
+      // Else, return nothing
+      return null;
+    });
+    // Send all the alerts
+    res.send(alerts);
+  } catch (err) {
+    next(err);
+  }
+}
 
 async function getHistory(req, res, next) {
   try {
