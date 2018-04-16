994e5afd1e7a2fa127793b6e65027be528b1be0d
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-16 21:51:09 +0200

    refactor: remove socket.io

--- src/app.js
+++ src/app.js
@@ -3,27 +3,13 @@
 const app = require('express')();
 // Set up http server with app
 const server = require('http').Server(app);
-// Instantiate socket.io with http serveur
-const io = require('socket.io')(server);
 const bodyParser = require('body-parser');
 const websiteRouter = require('./website/websiteRouter');
 const monitoringRouter = require('./monitoring/monitoringRouter');
 const logRouter = require('./log/logRouter');
-const StatisticsManager = require('./statistics/StatisticsManager');
 
 // Use body parser for parsing the body of http requests to json objets
 app.use(bodyParser.json());
-
-io.on('connection', (socket) => {
-  console.log('a user connected');
-  const statisticsManager = new StatisticsManager(socket);
-  statisticsManager.startStatisticsComputing(10000, 600000);
-  statisticsManager.startStatisticsComputing(60000, 3600000);
-  socket.on('disconnect', () => {
-    statisticsManager.stopStatisticsComputing();
-    console.log('user disconnected');
-  });
-});
 
 app.use('/api/website', websiteRouter);
 app.use('/api/monitoring', monitoringRouter);
