5728d17396dba0f2687e2ba1db3a4acd05275be1
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-17 15:43:41 +0200

    feat: add AlertManager

---
+++ src/history/AlertManager.js
@@ -0,0 +1,39 @@
+// Import node modules
+const { Website } = require('../database/database');
+
+class StatisticsManager {
+  constructor(socket) {
+    // This variable keeps track of the socket for sending live statistics
+    this.socket = socket;
+    // This variable keeps track of running intervals
+    this.intervals = [];
+  }
+
+  sendAlert(alert) {
+    console.log('Alert sent !');
+    this.socket.emit('alert', alert);
+  }
+
+  // This function set up check of availability every timestamp
+  async startWatching(timestamp, duration) {
+  // Get all websites
+    const websites = await Website.find().select('name');
+    // Check website availability at regular intervals, given by the checkInterval field
+    websites.map((website) => {
+      const interval = setInterval(checkAvailability, timestamp, website.name, duration, this);
+      // Keep track of running intervals to be able to cancel them later
+      this.intervals.push(interval);
+    });
+  }
+
+  // This function stops computing statistics
+  stopWatching() {
+  // Cancel all running intervals
+    this.intervals.map((interval) => {
+      clearInterval(interval);
+    });
+  }
+}
+
+module.exports = StatisticsManager;
+
