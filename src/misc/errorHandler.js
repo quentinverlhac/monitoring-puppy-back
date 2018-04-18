31b8c72ded4b79fba67d52455c958a179e9b17b5
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-18 22:04:18 +0200

    doc: add doc in error handler

--- src/misc/errorHandler.js
+++ src/misc/errorHandler.js
@@ -1,4 +1,4 @@
-// Display the error
+// If an unhandled error occurs during the request, send an error message to front
 function handleError(err, req, res, next) {
   console.error(err.stack);
   res.status(500).send('Error: something went wrong dealing with your request');
