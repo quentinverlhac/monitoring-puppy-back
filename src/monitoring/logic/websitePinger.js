504023431443ed218acc24f0c9b6141941de38f4
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-21 12:25:53 +0200

    doc: website pinger doc

--- src/monitoring/logic/websitePinger.js
+++ src/monitoring/logic/websitePinger.js
@@ -2,44 +2,53 @@
 const axios = require('axios');
 const { Log } = require('../../database/database');
 
-// This function sends a request to the specified website to check if it is available.
+// This function sends a request to the specified website to check if it is available
 function pingWebsite(website) {
   try {
-    // Save beginning time to compute response time.
+    // Save beginning time to compute response time
     const beginningTime = Date.now();
-    // Send the request with a timeout of 10 seconds.
+    // Send the request with a timeout of 10 seconds
     axios.get(website.url, { timeout: 10000 })
       .then(async (response) => {
-        // Success, everything is fine
+        // Successful response, everything is fine
+        // Get the reception time to compute response time
         const receptionTime = Date.now();
         // Add the information to the website check list
+        // Success field is set to true to show the response was successful
         const log = new Log({
           website: website._id,
           dateTimestamp: receptionTime,
-          answered: true,
+          success: true,
           responseTime: receptionTime - beginningTime,
           responseCode: response.status,
         });
+        // Save the log in database
         await log.save();
       })
       .catch(async (error) => {
-        // There is an error
+        // There is an error:
+        // Either the website sent an error response or it didn't send any response
+        // Get the reception time to compute response time, if there is a response
         const receptionTime = Date.now();
         let responseCode;
         let responseTime;
         if (error.response) {
-          // If there is a response, get its code and its time
+          // If there is a response, get its code and compute its response time
           responseCode = error.response.status;
           responseTime = receptionTime - beginningTime;
         }
         // Add the information to the website check list
+        // Success field is set to false to show the response wasn't successful
+        // If there wasn't any response, responseTime and responseCode will be undefined
+        // Their fields in the database will be empty
         const log = new Log({
           website: website._id,
           dateTimestamp: receptionTime,
-          answered: false,
+          success: false,
           responseTime,
           responseCode,
         });
+        // Save the log in database
         await log.save();
       });
   } catch (err) {
