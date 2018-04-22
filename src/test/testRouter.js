c64865f051f1c642a27de696d7a06535e5039914
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-22 10:29:54 +0200

    doc: api test router

--- src/test/testRouter.js
+++ src/test/testRouter.js
@@ -11,3 +11,18 @@ router.route('/random').get(randomResponse);
 
 module.exports = router;
 
+/**
+ * @api {get} api/test/cos cosResponse
+ * @apiName cosResponse
+ * @apiGroup test
+ * @apiDescription A route that randomly send successful or error responses, following a cosinus threshold. It simulates a website that periodically goes down.
+ * @apiVersion 0.1.0
+ */
+
+/**
+ * @api {get} api/test/random randomResponse
+ * @apiName randomResponse
+ * @apiGroup test
+ * @apiDescription A route that send randomly chosen status codes. It is used to test the count of status codes.
+ * @apiVersion 0.1.0
+ */
