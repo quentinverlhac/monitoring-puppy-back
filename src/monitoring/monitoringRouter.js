1371b68712729284ba82110d54d2d782e678efe5
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-22 10:36:23 +0200

    doc: monitoring router

--- src/monitoring/monitoringRouter.js
+++ src/monitoring/monitoringRouter.js
@@ -20,3 +20,21 @@ router.use(handleError);
 module.exports = router;
 
 // Write API documentation
+
+/**
+ * @api {get} api/monitoring/run runMonitoring
+ * @apiName runMonitoring
+ * @apiGroup monitoring
+ * @apiDescription Start the monitoring of websites in background. Websites will be pinged regularly, but statistics and alerts will not be sent.
+ * @apiSuccess (Response field) {String} data A string saying 'Websites are now regularly ping'
+ * @apiVersion 0.1.0
+ */
+
+/**
+ * @api {get} api/monitoring/stop stopMonitoring
+ * @apiName stopMonitoring
+ * @apiGroup monitoring
+ * @apiDescription Stop the monitoring of websites in background. Websites will stop being pinged regularly.
+ * @apiSuccess (Response field) {String} data A string saying 'Websites ping stopped'
+ * @apiVersion 0.1.0
+ */
