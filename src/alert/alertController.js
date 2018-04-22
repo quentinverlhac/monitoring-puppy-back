bef6c4f14c8ab605a04c04846204aaec84c7eb19
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-22 10:54:13 +0200

    doc: alert controller

--- src/alert/alertController.js
+++ src/alert/alertController.js
@@ -1,6 +1,7 @@
 // Import node modules
 const { Alert } = require('../database/database');
 
+// This function get the history from the given timestamp date to the current date
 async function getHistory(req, res, next) {
   try {
     // Get all history until date timestamp
