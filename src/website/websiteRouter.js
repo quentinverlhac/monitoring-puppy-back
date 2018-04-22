2cbcc89d10d8e788bd31772bf8787056c6c10923
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-22 10:52:10 +0200

    doc: website router

--- src/website/websiteRouter.js
+++ src/website/websiteRouter.js
@@ -58,7 +58,8 @@ module.exports = router;
  * @apiName updateWebsite
  * @apiGroup Website
  * @apiDescription Update the url named website
- * @apiParam (Body parameters) {String} [name] The name of the website (must be unique)
+ * @apiParam (URL parameters) {String} name The current name of the website
+ * @apiParam (Body parameters) {String} [name] The new name of the website (must be unique)
  * @apiParam (Body parameters) {String} [url] The url of the website (must be unique)
  * @apiParam (Body parameters) {Number} [checkInterval] The time interval between each availability check (in seconds)
  * @apiSuccess (Response field) {Object} data The response is an object
@@ -71,6 +72,7 @@ module.exports = router;
  * @apiName deleteWebsite
  * @apiGroup Website
  * @apiDescription Delete the url named website
+ * @apiParam (URL parameters) {String} name The current name of the website
  * @apiSuccess (Response field) {Object} data The response is an object
  * @apiUse responseWebsite
  * @apiVersion 0.1.0
