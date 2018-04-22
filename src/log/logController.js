9cfdd2a140450b38155dd52643f3f37102d5eaee
Quentin VERLHAC <quentin.verlhac@student.ecp.fr>
2018-04-22 10:51:23 +0200

    doc: logController

--- src/log/logController.js
+++ src/log/logController.js
@@ -1,6 +1,8 @@
 // Import node modules
 const logGetter = require('../log/logGetter');
 
+// This function gets a website logs between a beginning and an end date
+// It uses the logGetter logic to handle the request
 async function getLogs(req, res, next) {
   try {
     const logs = await logGetter(req.params.website, req.params.beginning, req.params.end);
